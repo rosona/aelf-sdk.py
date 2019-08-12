2dbb5a1b025cf238310d1c88b2d4e383faf8963d
peng <rongpeng@live.com>
2019-08-12 15:51:30 +0800

    Update sdk name

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='aelf',
+    name='aelf-sdk',
     version='0.1',
     description='Python SDK for AElf',
-    scripts=['helloworld'],
     url='https://github.com/rosona/aelf-sdk.py',
-    install_requires=['base58', 'requests', 'coincurve', 'protobuf']
+    install_requires=['requests', 'protobuf', 'base58', 'coincurve']
 )
